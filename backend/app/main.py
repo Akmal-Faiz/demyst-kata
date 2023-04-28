f3f22169dd573e0aa0fae8eb9d6178db030374fc
akmal <abinomar@dxc.com>
2023-04-28 22:08:10 +0800

    custom error handler

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,36 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from app.api.routes import loan_application
+from starlette.requests import Request
+from starlette.responses import Response
+
 
 app = FastAPI()
 
 app.include_router(loan_application.router)
 
 # Configure CORS middleware
+
+# CORS and custom error handling ref: https://github.com/tiangolo/fastapi/issues/775#issuecomment-592946834
+
+
+async def catch_exceptions_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except ValueError as exc:
+        return JSONResponse(
+            status_code=400,
+            content={"detail": str(exc)},
+        )
+    except Exception as exc:
+        return JSONResponse(
+            status_code=500,
+            content={"detail": str(exc)},
+        )
+
+app.middleware('http')(catch_exceptions_middleware)
+
+
 origins = [
     "*"
 ]
@@ -20,9 +44,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.exception_handler(Exception)
-async def http_exception_handler(request, exc):
-    return JSONResponse(
-        status_code=500,
-        content={"detail": str(exc)},
-    )
+# @app.exception_handler(Exception)
+# async def http_exception_handler(request, exc):
+#     return JSONResponse(
+#         status_code=500,
+#         content={"detail": str(exc)},
+#     )
