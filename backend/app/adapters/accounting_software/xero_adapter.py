0fd6054113622899803a47e2d3fe4d078492a699
akmal <abinomar@dxc.com>
2023-04-30 19:26:25 +0800

    changed balance sheet data for xero

--- backend/app/adapters/accounting_software/xero_adapter.py
+++ backend/app/adapters/accounting_software/xero_adapter.py
@@ -8,74 +8,74 @@ class XeroAdapter(AbstractAccountingAdapter):
             {
                 "year": 2020,
                 "month": 12,
-                "profitOrLoss": 250000,
-                "assetsValue": 1234
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             },
             {
                 "year": 2020,
                 "month": 11,
-                "profitOrLoss": 1150,
-                "assetsValue": 5789
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             },
             {
                 "year": 2020,
                 "month": 10,
-                "profitOrLoss": 2500,
-                "assetsValue": 22345
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             },
             {
                 "year": 2020,
                 "month": 9,
-                "profitOrLoss": -187000,
-                "assetsValue": 223452
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             },
             {
                 "year": 2020,
                 "month": 8,
-                "profitOrLoss": 250000,
-                "assetsValue": 1234
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             },
             {
                 "year": 2020,
                 "month": 7,
-                "profitOrLoss": 1150,
-                "assetsValue": 5789
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             },
             {
                 "year": 2020,
                 "month": 6,
-                "profitOrLoss": 2500,
-                "assetsValue": 22345
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             },
             {
                 "year": 2020,
                 "month": 5,
-                "profitOrLoss": -187000,
-                "assetsValue": 223452
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             },
             {
                 "year": 2020,
                 "month": 4,
-                "profitOrLoss": 250000,
-                "assetsValue": 1234
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             },
             {
                 "year": 2020,
                 "month": 3,
-                "profitOrLoss": 1150,
-                "assetsValue": 5789
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             },
             {
                 "year": 2020,
                 "month": 2,
-                "profitOrLoss": 2500,
-                "assetsValue": 22345
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             },
             {
                 "year": 2020,
                 "month": 1,
-                "profitOrLoss": -187000,
-                "assetsValue": 223452
+                "profitOrLoss": -100,
+                "assetsValue": 5000
             }
         ]
         
